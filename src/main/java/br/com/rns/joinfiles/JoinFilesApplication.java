de287e07312535eeed41affd48474074b7d51bcd
rafaelns <ns.raphael@gmail.com>
2022-12-11 20:55:27 -0300

    arquivos do projeto

---
+++ src/main/java/br/com/rns/joinfiles/JoinFilesApplication.java
@@ -0,0 +1,11 @@
+package br.com.rns.joinfiles;
+
+import br.com.rns.joinfiles.core.ProcessFiles;
+
+public class JoinFilesApplication {
+
+    public static void main(String[] args)  {
+        new ProcessFiles().process();
+    }
+
+}
